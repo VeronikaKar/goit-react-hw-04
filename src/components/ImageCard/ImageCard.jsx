28cdf42b98db8d460c0db3933995a3dbe8e1f2f6
VeronikaKaraul <veronika.life@gmail.com>
2024-06-19 17:40:51 +0300

    add image styles

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -4,7 +4,7 @@ export function ImageCard({ src, alt, onClick, src_regular }) {
   return (
     <div className={s.wrapper}>
       <img
-        className={s.img__card}
+        className={s.imgCard}
         onClick={() => onClick(src_regular, alt)}
         src={src}
         alt={alt}
